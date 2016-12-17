c9d6e35141068a67af3da844c6e352125f342374
Diego Santos Seabra <dss-diego@hotmail.com>
2016-12-17 17:41:40 -0200

    First Commit

---
+++ CalculoVazao.py
@@ -0,0 +1,30 @@
+#--------------------------------------------------
+#   Trabalho de Automação Industrial
+#   Software Desenvolvido para Calculo da Vazão
+#   Autores:
+#       Guilherme Guedes,
+#       Larissa Rodrigues,
+#       Lucas Lemos,
+#       Marcelo Andrade,
+#       Stevan Chaves
+#--------------------------------------------------
+
+#--------------------------------------------------
+#   Descrição de Uso de Variáveis
+#
+#   "v_" -> Se refere à variáveis de entrada do software
+#   "r_" -> Se refere à variáveis de saída do software
+#
+#--------------------------------------------------
+
+# Importando a biblioteca pandas (Python Data Analysis Library) para leitura do arquivo ".csv"
+import pandas as pd
+
+# Realizando a leitura do arquivo ".csv"
+# csv = pd.read_csv(csv_file)
+
+# v_Pressao = csv.Pressao
+# v_Temperatura = csv.Temperatura
+
+# r_Escoamento =
+# r_Massa =
